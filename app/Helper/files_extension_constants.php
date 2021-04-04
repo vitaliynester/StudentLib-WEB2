65cc4d0c3810418f2ecc9392e6c2667eb2423827
Vitaly Posadnev <elemabor@gmail.com>
2021-04-05 01:29:02 +0300

    create helped files

---
+++ app/Helper/files_extension_constants.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "doc" => "application/msword",
+    "docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "xls" => "application/vnd.ms-excel",
+    "xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "jpg" => "image/jpeg",
+    "pdf" => "application/pdf",
+    "png" => "image/png",
+    "zip" => "application/zip"
+];
---
+++ app/Helper/files_type_constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "Архивы" => "zip",
+    "Документы" => "doc,docx,xls,xlsx,pdf",
+    "Изображения" => "jpg,png",
+    "Остальное" => "other"
+];
